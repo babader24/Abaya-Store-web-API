46defcf0f5e2a72773e2078dd523301460b42dbe
babader24 <ahmedbabader7@gmail.com>
2025-05-02 17:12:38 +0300

    add the ServicesRegistration for presistences

---
+++ Abaya_Store.Presistence/Repositories/WishListRepository.cs
@@ -0,0 +1,15 @@
+﻿using Abaya_Store.Application.Persistence.Contracts;
+using Abaya_Store.Domain.Entities;
+using Abaya_Store.Persistence;
+
+namespace Abaya_Store.Presistence.Repositories
+{
+	public class WishListRepository : GenaricRepository<WishList>, IWishListRepository
+	{
+		private readonly AppDbContext _context;
+		public WishListRepository(AppDbContext context) : base(context)
+		{
+			_context = context;
+		}
+	}
+}
--- obj/Debug/net9.0/Abaya_Store.AssemblyInfo.cs
+++ obj/Debug/net9.0/Abaya_Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Abaya_Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114ee6d3e283bfde6709c24e8730935254547942")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27aca2a99399599740a2ebfc377487d25a6fe2c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Abaya_Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Abaya_Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
